57c4b30636bbf664b15f0a204a40be6955c964be
vanishree226 <121443678+vanishree226@users.noreply.github.com>
2023-01-17 22:20:25 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
-// const sound = document.getElementById("sound");
+const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
