9975279d7aa53540e3663a22b0c488f8f9649393
vanishree226 <121443678+vanishree226@users.noreply.github.com>
2023-01-17 22:07:04 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
-const sound = document.getElementById("sound");
+// const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
@@ -10,9 +10,9 @@ btn.addEventListener("click", () => {
             result.innerHTML = `
             <div class="word">
                     <h3>${inpWord}</h3>
-//                     <button onclick="playSound()">
-//                         <i class="fas fa-volume-up"></i>
-//                     </button>
+                   <button onclick="playSound()">
+                     <i class="fas fa-volume-up"></i>
+                  </button>
                 </div>
                 <div class="details">
                     <p>${data[0].meanings[0].partOfSpeech}</p>
